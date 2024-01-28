88d13dcacc37c21f0db85f286f46b639e67185bb
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-01-28 16:17:51 +0800

    feat: 🎸 integrate `@svgr/webpack` setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {}
+  env: {},
+  webpack: config => {
+    // Grab the existing rule that handles SVG imports
+    const fileLoaderRule = config.module.rules.find(rule =>
+      rule.test?.test?.('.svg')
+    )
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/ // *.svg?url
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+        use: ['@svgr/webpack']
+      }
+    )
+
+    fileLoaderRule.exclude = /\.svg$/i
+
+    return config
+  }
 }
 
 export default nextConfig
