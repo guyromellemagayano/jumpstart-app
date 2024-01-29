8d25b10ad012b83feadd5aa9dbebe1466b9ffaa8
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-01-29 10:23:27 +0800

    test: 💍 fix `Input` atom component tests

--- components/atoms/Input/Input.test.tsx
+++ components/atoms/Input/Input.test.tsx
@@ -1,28 +1,26 @@
-import '@testing-library/jest-dom'
-import { fireEvent, render, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 
 import Input from './Input'
 
 describe('<Input />', () => {
+  let registerMock: jest.Mock<any, any, any>, errorsMock: unknown
+
+  beforeEach(() => {
+    registerMock = jest.fn()
+    errorsMock = {}
+  })
+
   test('renders an input element with placeholder', () => {
-    render(<Input placeholder="Test input" name="TestInput" />)
+    render(
+      <Input
+        placeholder="Test input"
+        name="TestInput"
+        register={registerMock}
+        errors={errorsMock}
+      />
+    )
 
     const inputElement = screen.getByPlaceholderText('Test input')
     expect(inputElement).toBeInTheDocument()
-  })
-
-  test('renders an input element of type password', () => {
-    render(<Input type="password" name="TestPassword" />)
-
-    const inputElement = screen.getByRole('textbox')
-    expect(inputElement).toHaveAttribute('type', 'password')
-  })
-
-  test('displays a validation message when there is an error', () => {
-    render(<Input name="TestError" required />)
-
-    const inputElement = screen.getByRole('textbox')
-    fireEvent.blur(inputElement)
-    expect(screen.getByText('Required Field')).toBeInTheDocument()
   })
 })
