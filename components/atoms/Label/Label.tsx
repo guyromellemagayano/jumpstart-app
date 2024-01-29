005c30e0dd02d192b03aa3c1cc0465487ed8998b
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-01-29 14:45:09 +0800

    fix: 🐛 remove CSS module in `Label` component

--- components/atoms/Label/Label.tsx
+++ components/atoms/Label/Label.tsx
@@ -1,10 +1,6 @@
 import { LabelHTMLAttributes } from 'react'
 
-import { clsx } from 'clsx'
-
 import { TCommonAdditionalProps, TCommonChildrenProps } from '@/types/common'
-
-import styles from './Label.module.css'
 
 export type TLabelProps = LabelHTMLAttributes<HTMLLabelElement> &
   TCommonChildrenProps &
@@ -19,7 +15,7 @@ export type TLabelProps = LabelHTMLAttributes<HTMLLabelElement> &
  */
 const Label = ({ className, children, ...rest }: TLabelProps): JSX.Element => {
   return (
-    <label className={clsx(styles.label, className)} {...rest}>
+    <label className={className} {...rest}>
       {children}
     </label>
   )
